e48961a4c21820d26ea07493fc0ef35c00c6afab
thangdac <nguyendac1606123@gmail.com>
2023-10-16 20:13:02 +0700

    text

--- BaiKiemTra_WinForm/BangCap.Designer.cs
+++ BaiKiemTra_WinForm/BangCap.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BaiKiemTra_WinForm
 {
-    partial class Form1
+    partial class BangCap
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1636, 562);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "BangCap";
         }
 
         #endregion
     }
 }
-
