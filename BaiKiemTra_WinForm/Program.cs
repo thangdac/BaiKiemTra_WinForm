14063e88ddb0039006424fc767c2a06178e8365b
thangdac <nguyendac1606123@gmail.com>
2023-10-18 23:12:49 +0700

    end 3

--- BaiKiemTra_WinForm/Program.cs
+++ BaiKiemTra_WinForm/Program.cs
@@ -16,7 +16,7 @@ namespace BaiKiemTra_WinForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BangCap());
+            Application.Run(new Form1());
         }
     }
 }
